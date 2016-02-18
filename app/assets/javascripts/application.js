20230c6da7bb861718afb37593c58dc0876f79cc
Steven Harley <smharley@gmail.com>
2016-02-17 16:21:40 -0800

    Update markers and clusters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,21 +4,3 @@
 //= require Control.Opacity
 //= require Leaflet.Coordinates
 //= require_tree .
-
-// var url = "http://s3-us-west-1.amazonaws.com/caltopo/topo/{z}/{x}/{y}.png?v=1";
-// L.tileLayer(url, {
-//   attribution: "USGS map from <a href='http://caltopo.com/'>CalTopo</a>",
-//   minZoom: 5,
-//   maxZoom: 18,
-//   maxNativeZoom: 14,
-//   opacity: 0.25
-// }).addTo(map);
-
-// var url = "http://s3-us-west-1.amazonaws.com/ctslope/relief/{z}/{x}/{y}.png?v=1";
-// L.tileLayer(url, {
-//   attribution: "USGS map from <a href='http://caltopo.com/'>CalTopo</a>",
-//   minZoom: 5,
-//   maxZoom: 18,
-//   maxNativeZoom: 14,
-//   opacity: 0.25
-// }).addTo(map);
